02d32729a7d34404ef86c60aafb6ff26027ec6d7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-16 23:04:54 +0200

    Initial Commit

---
+++ GroceryShopperSwiftUIandRealm/GroceryShopperSwiftUIandRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryShopperSwiftUIandRealmApp.swift
+//  GroceryShopperSwiftUIandRealm
+//
+//  Created by ramil on 16.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct GroceryShopperSwiftUIandRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
